5a609a258d295d79bb59b516dea931cf3fd66738
Hamid <hamidthedev@gmail.com>
2025-07-28 17:06:24 +0600

    baseurl udpated

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -1,6 +1,6 @@
 import { createAuthClient } from 'better-auth/react'
 export const authClient = createAuthClient({
-	baseURL: process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:3000',
+	baseURL: process.env.NEXT_PUBLIC_BASE_URL || 'https://block-remit.vercel.app',
 })
 
 export const { signIn, signUp, useSession, signOut } = authClient
