1689a2cf44f4ac11a50e5d5a181178830a08a6ad
Hamid <hamidthedev@gmail.com>
2025-07-28 16:41:52 +0600

    ts ignore during build

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+	/* config options here */
+	typescript: {
+		ignoreBuildErrors: true,
+	},
+	images: {
+		unoptimized: true,
+	},
+	reactStrictMode: true,
+}
 
-export default nextConfig;
+export default nextConfig
